f66e6406e0997718cd34077c89623a6e2908410e
Rohit-singh19 <rohitsingh19032001@gmail.com>
2021-07-25 03:14:09 +0530

    Youtube source Code

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const toggleMenu = document.querySelector('.toggle-menu');
-      const sideBar = document.querySelector('.sidebar');
-      const menuIcon = document.querySelector('.toggle-menu i');
+const sideBar = document.querySelector('.sidebar');
+const menuIcon = document.querySelector('.toggle-menu i');
 
-      toggleMenu.addEventListener('click', ()=>{
-          sideBar.classList.toggle('show');
-          toggleMenu.classList.toggle('margin');
-          if(menuIcon.className == 'fas fa-bars'){
-              menuIcon.className = 'fas fa-times';
-            }
-            else if(menuIcon.className == 'fas fa-times'){
-                menuIcon.className = 'fas fa-bars';
-          }
-      });
+
+toggleMenu.addEventListener('click', () =>{
+    sideBar.classList.toggle('show');
+    toggleMenu.classList.toggle('margin');
+    if(menuIcon.className == 'fas fa-bars'){
+        menuIcon.className = 'fas fa-times';
+    }
+    else if(menuIcon.className == 'fas fa-times'){
+        menuIcon.className = 'fas fa-bars';
+    }
+});
--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const hero = document.querySelector('.hero');
 
 var i = 0;
-setInterval(() => {
 
+setInterval(() => {
     const image = [
-    'url("Images/slideshow1.jpg")',
-    'url("Images/slideshow2.jpg")'
+        'url("Images/slideshow1.jpg")',
+        'url("Images/slideshow2.jpg")',
     ]
-    // const bg = image[i];
 
     hero.style.backgroundImage = image[i];
     hero.style.opacity = "1";
-    (i==0)? i = 1:i = 0;
-    // console.log(i);
-    
 
+    (i == 0)? i = 1:i = 0;
 }, 4000);
-
